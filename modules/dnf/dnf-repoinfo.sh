632aa9ea61c600e5f99d824561f4666e575c7570
certifiedfoolio <156134535+cherry-os@users.noreply.github.com>
2025-03-12 16:11:56 -0700

    Update dnf-repoinfo.sh

--- modules/dnf/dnf-repoinfo.sh
+++ modules/dnf/dnf-repoinfo.sh
@@ -16,52 +16,74 @@ fi
 
 echo "{"
 echo "  \"$repo_id\": {"
-  name=$(echo "$repo_info" | grep -oP "^Repo-name *: *\K.*")
-  if [ -n "$name" ]; then
-    echo "    \"name\": \"$name\","
+
+  repo_id_val=$(echo "$repo_info" | grep -oP "^Repo-id *: *\K.*")
+  if [ -n "$repo_id_val" ]; then
+    echo "    \"repo-id\": \"$repo_id_val\","
   fi
 
-  baseurl=$(echo "$repo_info" | grep -oP "^Repo-baseurl *: *\K.*")
-  if [ -n "$baseurl" ]; then
-    echo "    \"baseurl\": \"$baseurl\","
+  repo_name=$(echo "$repo_info" | grep -oP "^Repo-name *: *\K.*")
+  if [ -n "$repo_name" ]; then
+    echo "    \"repo-name\": \"$repo_name\","
   fi
 
-  enabled=$(echo "$repo_info" | grep -oP "^Repo-status *: *\K.*")
-  if [ -n "$enabled" ]; then
-    if [[ "$enabled" == "enabled" ]]; then
-        echo "    \"enabled\": true,"
+  repo_status=$(echo "$repo_info" | grep -oP "^Repo-status *: *\K.*")
+  if [ -n "$repo_status" ]; then
+    if [[ "$repo_status" == "enabled" ]]; then
+      echo "    \"repo-status\": true,"
     else
-        echo "    \"enabled\": false,"
+      echo "    \"repo-status\": false,"
     fi
   fi
 
-  gpgcheck=$(echo "$repo_info" | grep -oP "^Repo-gpgcheck *: *\K.*")
-  if [ -n "$gpgcheck" ]; then
-    if [[ "$gpgcheck" == "enabled" ]]; then
-        echo "    \"gpgcheck\": true,"
-    else
-        echo "    \"gpgcheck\": false,"
-    fi
+  repo_revision=$(echo "$repo_info" | grep -oP "^Repo-revision *: *\K.*")
+  if [ -n "$repo_revision" ]; then
+    echo "    \"repo-revision\": \"$repo_revision\","
   fi
 
-  gpgkey=$(echo "$repo_info" | grep -oP "^Repo-gpgkey *: *\K.*")
-  if [ -n "$gpgkey" ]; then
-    echo "    \"gpgkey\": [$(echo "$repo_info" | grep -oP "^Repo-gpgkey *: *\K.*" | tr ' ' ','| sed 's/,$//')],"
-  fi
-  
-  metalink=$(echo "$repo_info" | grep -oP "^Repo-metalink *: *\K.*")
-  if [ -n "$metalink" ]; then
-    echo "    \"metalink\": \"$metalink\","
+  repo_updated=$(echo "$repo_info" | grep -oP "^Repo-updated *: *\K.*")
+  if [ -n "$repo_updated" ]; then
+    echo "    \"repo-updated\": \"$repo_updated\","
   fi
 
-  mirrorlist=$(echo "$repo_info" | grep -oP "^Repo-mirrorlist *: *\K.*")
-  if [ -n "$mirrorlist" ]; then
-    echo "    \"mirrorlist\": \"$mirrorlist\","
+  repo_pkgs=$(echo "$repo_info" | grep -oP "^Repo-pkgs *: *\K.*")
+  if [ -n "$repo_pkgs" ]; then
+    echo "    \"repo-pkgs\": $repo_pkgs,"
   fi
 
-  count=$(echo "$repo_info" | grep -oP "^Repo-packages *: *\K.*")
-  if [ -n "$count" ]; then
-    echo "    \"packages\": $count"
+  repo_available_pkgs=$(echo "$repo_info" | grep -oP "^Repo-available-pkgs *: *\K.*")
+  if [ -n "$repo_available_pkgs" ]; then
+    echo "    \"repo-available-pkgs\": $repo_available_pkgs,"
+  fi
+
+  repo_size=$(echo "$repo_info" | grep -oP "^Repo-size *: *\K.*")
+  if [ -n "$repo_size" ]; then
+    echo "    \"repo-size\": \"$repo_size\","
+  fi
+
+  repo_metalink=$(echo "$repo_info" | grep -oP "^Repo-metalink *: *\K.*")
+  if [ -n "$repo_metalink" ]; then
+    echo "    \"repo-metalink\": \"$repo_metalink\","
+  fi
+
+  updated=$(echo "$repo_info" | grep -oP "^Updated *: *\K.*")
+  if [ -n "$updated" ]; then
+    echo "    \"updated\": \"$updated\","
+  fi
+
+  repo_baseurl=$(echo "$repo_info" | grep -oP "^Repo-baseurl *: *\K.*")
+  if [ -n "$repo_baseurl" ]; then
+    echo "    \"repo-baseurl\": \"$repo_baseurl\","
+  fi
+
+  repo_expire=$(echo "$repo_info" | grep -oP "^Repo-expire *: *\K.*")
+  if [ -n "$repo_expire" ]; then
+    echo "    \"repo-expire\": \"$repo_expire\","
+  fi
+
+  repo_filename=$(echo "$repo_info" | grep -oP "^Repo-filename *: *\K.*")
+  if [ -n "$repo_filename" ]; then
+    echo "    \"repo-filename\": \"$repo_filename\""
   fi
 
   if [[ "$(tail -c 2 <<< "$(echo "$repo_info")" | head -c 1)" == "," ]]; then
