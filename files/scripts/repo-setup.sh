c5ac4e13764eabcdff0509a48a18f7c0febab8ee
certifiedfoolio <156134535+cherry-os@users.noreply.github.com>
2025-03-13 09:37:52 -0700

    AAAAAAAAAAAAAAAAAA

--- files/scripts/repo-setup.sh
+++ files/scripts/repo-setup.sh
@@ -4,4 +4,4 @@ set -oeu pipefail
 
 dnf -y config-manager --set-enabled crb
 dnf -y install epel-release
-dnf -y config-manager --addrepo https://copr.fedorainfracloud.org/coprs/chronos/cherries/repo/epel-10/chronos-cherries-epel-10.repo
+dnf -y config-manager --add-repo https://copr.fedorainfracloud.org/coprs/chronos/cherries/repo/epel-10/chronos-cherries-epel-10.repo
