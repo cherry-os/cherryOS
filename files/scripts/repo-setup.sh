f6ae55febcbfa16a74e6f7f2da7881a2ea17d311
certifiedfoolio <156134535+cherry-os@users.noreply.github.com>
2025-03-13 09:15:32 -0700

    Update repo-setup.sh

--- files/scripts/repo-setup.sh
+++ files/scripts/repo-setup.sh
@@ -3,6 +3,6 @@
 set -oeu pipefail
 
 dnf -y config-manager --set-enabled crb
-dnf -y install epel-release
+dnf -y install epel-release wget
 wget -O /etc/yum.repos.d/chronos-cherries-epel-%OS_VERSION%.repo \
     https://copr.fedorainfracloud.org/coprs/chronos/cherries/repo/epel-%OS_VERSION%/chronos-cherries-epel-%OS_VERSION%.repo
